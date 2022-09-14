44f7556fdafa143f1422eb5e1e62d68d07f441e3
agusMz1908 <agus.muniz1997@gmail.com>
2022-09-13 23:00:06 -0300

    1.1

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -6,11 +6,11 @@ const Nav = () => {
   return (
     <div className="flex flex-col pt-3 text-white">
         <nav className="flex text-xl items-center justify-around">
-            <a href="">Proyects</a>
-            <a href="">Tecnologys</a>
-            <a href="">About Me</a>
-            <a href="https://www.linkedin.com/in/agustin-muniz-02577220a/" target="_blank"><i className="fa-brands fa-linkedin text-white"></i></a>
-            <a href="https://github.com/agusMz1908" target="_blank"><i className="fa-brands fa-github text-white"></i></a>
+            <a href="" className="hover:text-red-500">Proyects</a>
+            <a href="" className="hover:text-red-500">Tecnologys</a>
+            <a href="" className="hover:text-red-500">About Me</a>
+            <a href="https://www.linkedin.com/in/agustin-muniz-02577220a/" target="_blank"><i className="fa-brands fa-linkedin text-white hover:text-red-500"></i></a>
+            <a href="https://github.com/agusMz1908" target="_blank"><i className="fa-brands fa-github text-white hover:text-red-500"></i></a>
         </nav>
     </div>
   )
